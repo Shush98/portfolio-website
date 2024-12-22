63d3307564e1171a0a96af2b0352b3ae4a8a1b0b
Shush98 <ghoshshushant00@gmail.com>
2024-12-21 22:35:46 -0500

    resolving isuues again

---
+++ app/dashboard/Credit@AI/page.tsx
@@ -0,0 +1,23 @@
+import { projectsData } from "@/lib/data";
+
+// const ProjectProps = (typeof projectsData)[number];
+
+export default function Page() {
+
+    const filteredData = projectsData.filter((item)=>(item.id=='Credit@AI'))
+
+    return (
+    <div>
+    {/* {projectsData.filter((item)=>(item.id===''))}     */}
+    {/* <p className="flex flex-col items-center"> */}
+        {filteredData.map((item,index)=>(
+        <div key={index}>
+        <li>{item.title}</li>
+        <li>{item.description}</li>
+        </div>
+        ))}
+        
+    {/* </p> */}
+    </div>
+    );
+  }
